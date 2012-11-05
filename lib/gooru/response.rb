08563d9caa2c55b603b55c8eef91578f40930213
Brandon Beacher <brandon.beacher@gmail.com>
2012-11-05 13:05:20 -0500

    Handle more Gooru error possibilities.

--- lib/gooru/response.rb
+++ lib/gooru/response.rb
@@ -1,16 +1,41 @@
 module Gooru
   class Response
     attr_reader :data
+    attr_reader :status
 
     def initialize(weary_response)
       @data = parse(weary_response.body)
+      @status = weary_response.status
+    end
+
+    def error?
+      has_error_key? ||
+      internal_server_error?
+    end
+
+    def error_message
+      if has_error_key?
+        data["error"]
+      elsif internal_server_error?
+        data["status"]
+      else
+        ""
+      end
     end
 
     def success?
-      data.has_key?("error") == false
+      !error?
     end
 
   private
+
+    def has_error_key?
+      data.has_key?("error")
+    end
+
+    def internal_server_error?
+      status == 500
+    end
 
     def parse(body)
       if body.match(/^error:(.*)/)
